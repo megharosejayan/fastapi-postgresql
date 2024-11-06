4cf7ee445f5a04825db4567ff219bb78c0a55c49
megharosejayan <megharose15@gmail.com>
2024-11-06 15:31:22 +0900

    Add: table and fields

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 'postgresql://postgres:megha@localhost:5434/QuizApplication'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
