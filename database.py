227a645eedd0e64b6212a4ddc793258f02f3960d
megharosejayan <megharose15@gmail.com>
2024-11-06 15:49:29 +0900

    Add: post endpoint

--- database.py
+++ database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# in real project store in env file, duh
 URL_DATABASE = 'postgresql://postgres:megha@localhost:5434/QuizApplication'
 
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
+
+Base = declarative_base()
